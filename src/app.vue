378f9cbf38a518efc40209447ff16ae80f4b2c5a
cam <webdevbycam@gmail.com>
2026-03-02 15:51:28 -0500

    Redirect root host to /link for link subdomain

--- src/app.vue
+++ src/app.vue
@@ -6,6 +6,15 @@
 
 <script setup lang="ts">
 // Layout global
+// if user visits root on the link subdomain, send them to the static folder
+if (process.client) {
+  if (
+    window.location.host === 'link.wondervillavicencio.com' &&
+    window.location.pathname === '/'
+  ) {
+    window.location.replace('/link/');
+  }
+}
 </script>
 
 <style lang="scss">
